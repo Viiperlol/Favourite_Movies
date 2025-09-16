13ac9a907abe95d9723f955e675a309ee7dbe707
Viiperlol <suhaybsheikh@hotmail.com>
2025-09-16 15:40:40 +0800

    commit

--- main.py
+++ main.py
@@ -13,14 +13,12 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 MOVIE_API_KEY = "02b2e2217debd7d354856c75efbec078"
-
-
-url = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 headers = {"accept": "application/json",
            "Authorization": "Bearer 02b2e2217debd7d354856c75efbec078"}
-
-response = requests.get(url, headers=headers)
 
 # CREATE DB
 class Base(DeclarativeBase):
@@ -39,9 +37,9 @@ class Movie(db.Model):
     title: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
     description: Mapped[str] = mapped_column(String(500), nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=False)
-    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
-    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 with app.app_context():
@@ -126,13 +124,36 @@ def delete():
         db.session.commit()
     return redirect(url_for('home'))
 
-
 @app.route("/add", methods=["GET", "POST"])
 def add():
     form = AddMovie()
-    movie_title = form.movie_title.data
-    print(movie_title)
-    return render_template("add.html", form=form, title=movie_title)
+    if form.validate_on_submit():
+        movie_title = form.movie_title.data
+
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+
+        # Redirect to /edit route
+        return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 if __name__ == '__main__':
