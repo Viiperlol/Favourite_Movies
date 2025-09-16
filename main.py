39e0e050855e10e2f785d7466547048c00d6b3b6
Viiperlol <suhaybsheikh@hotmail.com>
2025-09-16 16:05:08 +0800

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -12,7 +14,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
-MOVIE_API_KEY = "02b2e2217debd7d354856c75efbec078"
+MOVIE_API_KEY = os.getenv("MOVIE_API_KEY")
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
